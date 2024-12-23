dccdf3343ab5c140107b210605a57d5becfd5650
Matt Garcia <strongpawcoding@gmail.com>
2024-12-23 11:08:03 -0600

    added base.html, confirmed it is working with mainpage. Also added style sheet.

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='transactions_views'),
+    path('', views.index, name='transactions_index'),
 ]
 
