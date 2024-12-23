da3548e547a342c6437846d165cb45eba901f292
Matt Garcia <strongpawcoding@gmail.com>
2024-12-23 12:03:24 -0600

    added models for transactions, updated gitignore to ignore database and migrations. Configured admin for better UX

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Account, Category, Transaction
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost_type')
+    list_filter = ('cost_type',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_date', 'amount', 'account', 'category')
+    list_filter = ('transaction_date', 'account', 'category')
+    search_fields = ('account__name', 'category__name')
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import choices
 
-# Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    FIXED = 'Fixed'
+    VARIABLE = "Variable"
+    COST_CHOICES = [
+        (FIXED, "Fixed Cost"),
+        (VARIABLE, "Variable"),
+    ]
+
+    name = models.CharField(max_length=50, unique=True)
+    cost_type = models.CharField(max_length=10, choices=COST_CHOICES, default=VARIABLE)
+
+    def __str__(self):
+        return f"{self.name} ({self.cost_type})"
+
+class Transaction(models.Model):
+    transaction_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.transaction_date}, {self.amount}, {self.account}"
